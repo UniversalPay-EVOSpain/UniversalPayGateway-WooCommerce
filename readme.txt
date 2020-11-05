8098bb1efa63537a10027ac84257d54be9d62b4e
UniversalPay <65346889+UniversalPay-EVOSpain@users.noreply.github.com>
2020-11-05 21:20:28 +0100

    Add files via upload

---
+++ readme.txt
@@ -0,0 +1,21 @@
+﻿=== Universalpay Gateway for WooCommerce ===
+
+Contributors: Javier Bartolome
+
+Tags: woocommerce, servired, universalpay, credit card, martercard, visa, ecommerce
+
+Requires at least: 3.5
+
+Tested up to: 5.4.2
+
+Stable tag: 1.1.9
+
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+
+Integración para WooCommerce del sistema de pago Universalpay
+
+
+== Description ==
+
---
+++ universalpay-gateway-woocommerce.php
@@ -0,0 +1,42 @@
+<?php
+/*
+Plugin Name: UniversalPay Gateway for WooCommerce
+Plugin URI: https://www.universalpay.es/
+Description: This plugins allows to users to include UniversalPay at payment method
+Author: jbartolome
+Version: 1.3.0
+Author URI: https://www.universalpay.es/
+Text Domain: universalpay_gw_woo
+Domain Path: /languages
+*/
+
+add_action( 'plugins_loaded', 'universalpay_plugins_loaded' );
+add_action( 'init', 'universalpay_inicio' );
+
+function universalpay_inicio() {
+	load_plugin_textdomain( "universalpay_gw_woo", false, dirname( plugin_basename( __FILE__ ) ) . '/languages' );
+}
+
+function universalpay_plugins_loaded() {
+	if ( !class_exists( 'WC_Payment_Gateway' ) ) exit;
+
+	include_once ('class-wc-universalpay-gateway.php');
+	
+	add_filter( 'woocommerce_payment_gateways', 'woocommerce_add_gateway_universalpay_gateway' );
+}
+
+function woocommerce_add_gateway_universalpay_gateway($methods) {
+	$methods[] = 'WC_universalpay_Gateway';
+	return $methods;
+}
+
+function universalpay_enqueue($hook) {
+	if ( 'woocommerce_page_wc-settings' != $hook ) {
+        return;
+    }
+
+    wp_enqueue_script( 'jquery-validate', plugin_dir_url( __FILE__ ) . 'js/jquery.validate.min.js', array( "jquery" ), "1.13.1", true );
+}
+add_action( 'admin_enqueue_scripts', 'universalpay_enqueue' );
+
+
